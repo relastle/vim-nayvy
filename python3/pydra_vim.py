012ff5ee3696970ce2e7b3b11fad7283acffec5a
Hiroki Konishi <relastle@gmail.com>
2020-02-29 19:11:41 +0900

    :four_leaf_clover: Refactor : move vim interface out of package

---
+++ python3/pydra_vim.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+import vim
+
+from pydra.importing.fixer import (
+    Fixer,
+)
+from pydra.importing.import_config import (
+    ImportConfig,
+)
+
+
+def pydra_import() -> None:
+    config = ImportConfig.of_jsonfile(
+        os.environ['HOME'] +
+        '/.config/pydra/config.json'
+    )
+    if config is None:
+        print('cannot load pydra config file', file=sys.stderr)
+        sys.exit(1)
+    fixer = Fixer(config)
+    lines = vim.current.buffer[:]
+    fixed_lines = fixer.fix_lines_with_flake8(lines)
+    vim.current.buffer[:] = fixed_lines
+    return
