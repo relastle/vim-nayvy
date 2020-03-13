d5b21e9b289517885488f7615e45083624b83ea7
Hiroki Konishi <relastle@gmail.com>
2020-03-13 15:27:09 +0900

    :leaves: Fix : bash script

--- vader_test.sh
+++ vader_test.sh
@@ -3,7 +3,7 @@ if [[ ! -e ./vader.vim ]]; then
     git clone https://github.com/junegunn/vader.vim
 fi
 
-export XDG_CONFIG_HOME="$(pwd)/configs"
+XDG_CONFIG_HOME="$(pwd)/configs"
 
 nvim -Es -u <(cat << EOF
 filetype off
@@ -11,4 +11,5 @@ set rtp+=$(pwd)/vader.vim
 set rtp+=$(pwd)
 filetype plugin indent on
 syntax enable
-EOF) -c 'Vader! tests/*'
+EOF
+) -c 'Vader! tests/*'
