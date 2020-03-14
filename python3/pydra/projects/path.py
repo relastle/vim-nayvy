92f93253a09b6249e5234a13cc43ad3971fe87c5
Hiroki Konishi <relastle@gmail.com>
2020-03-14 10:49:33 +0900

    :ear_of_rice: Update : define moduel path fields

--- python3/pydra/projects/path.py
+++ python3/pydra/projects/path.py
@@ -1,11 +1,21 @@
 
-from .modules.models import Module  # noqa
+from typing import Optional
+from dataclasses import dataclass
+
+from .modules.models import Module
 
 
+@dataclass(frozen=True)
 class ModulePath:
     """ Represent one module.
 
     - Content of module
     - Path within python project
     """
-    pass
+
+    mod_path: str
+    mod: Module
+
+    @classmethod
+    def of_filepath(cls, filepath: str) -> Optional['ModulePath']:
+        pass
