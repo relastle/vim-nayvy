556284d8bad57166290d559877fc9a86d35f2eec
Hiroki Konishi <relastle@gmail.com>
2020-03-14 13:55:35 +0900

    :ear_of_rice: Update : implmented helper class for importing module inside the same package

--- python3/pydra/projects/path.py
+++ python3/pydra/projects/path.py
@@ -1,11 +1,13 @@
 
-from typing import Optional
+from typing import List, Optional
 from os.path import abspath, relpath
 from dataclasses import dataclass
 
 from . import get_pyproject_root
 from .modules.loader import ModuleLoader
 from .modules.models import Module
+from ..importing.fixer import ImportStatementMap
+from ..importing.import_statement import SingleImport
 
 
 @dataclass(frozen=True)
@@ -52,4 +54,82 @@ class ModulePath:
         return ModulePath(
             rel_path.replace('/', '.').rstrip('.py'),
             mod,
+        )
+
+
+def mod_relpath(target_modpath: str, base_modpath: str) -> str:
+    """ get relative path to `target_modpath` from `base_modpath`
+
+    i.g.
+        Args:
+            `target_modpath`: pydra.importing.import_statement
+            `base_modpath`: pydra.projects.path
+        Returns:
+            ..importing.import_statement
+    """
+    if target_modpath == base_modpath:
+        return '.'
+
+    target_path_elms = target_modpath.split('.')
+    base_path_elms = base_modpath.split('.')
+    if target_path_elms[0] != base_path_elms[0]:
+        return target_modpath
+    common_elm_num = 0
+    for target_elm, base_elm in zip(target_path_elms, base_path_elms):
+        if target_elm != base_elm:
+            break
+        common_elm_num += 1
+    return (
+        '.' * (len(base_path_elms) - common_elm_num) +
+        '.'.join(target_path_elms[common_elm_num:])
+    )
+
+
+@dataclass
+class ProjectImportHelper(ImportStatementMap):
+    """ Importing helper that providing import within project.
+    """
+
+    current_modpath: ModulePath
+    all_modpaths: List[ModulePath]
+    prefers_relative: bool
+
+    def _make_stmt_relative(self, modpath_target: ModulePath) -> str:
+        if not self.prefers_relative:
+            return modpath_target.mod_path
+        return mod_relpath(
+            modpath_target.mod_path,
+            self.current_modpath.mod_path,
+        )
+
+    def __getitem__(self, name: str) -> Optional[SingleImport]:
+        for modpath in self.all_modpaths:
+            stmt = modpath.get_import(name)
+            if stmt is not None:
+                return SingleImport(
+                    name,
+                    'from {} import {}'.format(
+                        self._make_stmt_relative(modpath),
+                        name,
+                    ),
+                    2,  # project level
+                )
+        return None
+
+    @classmethod
+    def of_filepath(
+        cls,
+        loader: ModuleLoader,
+        filepath: str,
+    ) -> Optional['ProjectImportHelper']:
+        current_modpath = ModulePath.of_filepath(
+            loader,
+            filepath,
+        )
+        if current_modpath is None:
+            return None
+        return ProjectImportHelper(
+            current_modpath,
+            [],
+            True,
         )
--- python3/tests/projects/test_path.py
+++ python3/tests/projects/test_path.py
@@ -1,10 +1,59 @@
 import unittest
 from os.path import dirname
 from pathlib import Path
+from dataclasses import dataclass
 
-from pydra.projects.path import ModulePath
-from pydra.projects.modules.models import Module, Function, Class
+from pydra.projects.path import (
+    ModulePath, mod_relpath,
+    ProjectImportHelper,
+)
 from pydra.projects.modules.loader import SyntacticModuleLoader
+from pydra.projects.modules.models import Class, Module, Function
+from pydra.importing.import_statement import SingleImport
+
+
+class Test(unittest.TestCase):
+
+    def test_mod_relpath(self) -> None:
+
+        @dataclass
+        class Case:
+            description: str
+            target_modpath: str
+            base_modpath: str
+            expected: str
+
+        for case in [
+            Case(
+                description='same mod path',
+                target_modpath='package.module',
+                base_modpath='package.module',
+                expected='.',
+            ),
+            Case(
+                description='mod in the same package',
+                target_modpath='package.module1',
+                base_modpath='package.module2',
+                expected='.module1',
+            ),
+            Case(
+                description='subpackage mod path',
+                target_modpath='package.subpackage.module',
+                base_modpath='package.module',
+                expected='.subpackage.module',
+            ),
+            Case(
+                description='different package ',
+                target_modpath='package.subpackage1.module',
+                base_modpath='package.subpackage2.module',
+                expected='..subpackage1.module',
+            ),
+        ]:
+            self.assertEqual(
+                mod_relpath(case.target_modpath, case.base_modpath),
+                case.expected,
+                case.description,
+            )
 
 
 class TestModulePath(unittest.TestCase):
@@ -66,3 +115,30 @@ class TestModulePath(unittest.TestCase):
             module_path.get_import('f3') is
             None
         )
+
+
+class TestProjectImportHelper(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sample_project_path = (
+            Path(dirname(__file__)) /
+            '..' /
+            '_resources' /
+            'sample_project'
+        )
+        return
+
+    def test___getitem__(self) -> None:
+        actual = ProjectImportHelper.of_filepath(
+            SyntacticModuleLoader(),
+            str(
+                self.sample_project_path /
+                'package' /
+                'main.py'
+            ))
+        assert actual is not None
+        actual['sub_top_level_function1'] == SingleImport(
+            'sub_top_level_function1',
+            'from .subpackage.sub_main import sub_top_level_function1',
+            2,
+        )
+        return
