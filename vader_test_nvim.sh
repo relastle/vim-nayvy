5c6e82ec9c2db9f2321059b75d661c626d349da2
Hiroki Konishi <relastle@gmail.com>
2020-03-16 18:12:46 +0900

    :ear_of_rice: Update : make sub runtimepaths under-score prefixed

--- vader_test_nvim.sh
+++ vader_test_nvim.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-if [[ ! -e ./vader.vim ]]; then
-    git clone https://github.com/junegunn/vader.vim
+if [[ ! -e ./_vader.vim ]]; then
+    git clone https://github.com/junegunn/vader.vim ./ _vader.vim
+fi
+
+if [[ ! -e ./_ultisnips ]]; then
+    git clone https://github.com/SirVer/ultisnips ./_ultisnips
 fi
 
 XDG_CONFIG_HOME="$(pwd)/configs"
 
 nvim -Es -u <(cat << EOF
 filetype off
-set rtp+=$(pwd)/vader.vim
+set rtp+=$(pwd)/_vader.vim
+set rtp+=$(pwd)/_ultisnips
 set rtp+=$(pwd)
 filetype plugin indent on
 syntax enable
