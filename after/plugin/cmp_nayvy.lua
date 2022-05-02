e45c72f978fc680253efcfc6ce4d0cd9d6e3ad8a
Hiroki Konishi <relastle@gmail.com>
2022-05-02 11:29:08 +0900

    fix:

--- after/plugin/cmp_nayvy.lua
+++ after/plugin/cmp_nayvy.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_command("python3 from nayvy_vim_if.config import CONFIG")
-local nayvy_cmp_enabled = vim.fn.pyeval('CONFIG.cmp_enabled')
+local nayvy_cmp_enabled = vim.fn.py3eval('CONFIG.cmp_enabled')
 if nayvy_cmp_enabled ~= 1 then
     return
 end
