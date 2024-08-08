722f28c9886d795329d3fc841e2487d75a767ebe
Hiroki Konishi <relastle@gmail.com>
2024-08-08 22:25:52 +0900

    update: improve lua cmp startup time

--- after/plugin/cmp_nayvy.lua
+++ after/plugin/cmp_nayvy.lua
@@ -1,7 +1,5 @@
-vim.api.nvim_command("python3 from nayvy_vim_if.config import CONFIG")
-local nayvy_cmp_enabled = vim.fn.py3eval('CONFIG.cmp_enabled')
-if nayvy_cmp_enabled ~= 1 then
-    return
+if vim.g.nayvy_cmp_enabled ~= 1 then
+	return
 end
 
-require('cmp').register_source('nayvy', require('cmp_nayvy').new())
+require("cmp").register_source("nayvy", require("cmp_nayvy").new())
